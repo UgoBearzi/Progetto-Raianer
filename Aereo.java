10c41234505d4c348ac43fa7ba0d208614bed511
UgoBearzi <168535189+UgoBearzi@users.noreply.github.com>
2025-04-26 17:51:43 +0200

    Added Aereo

---
+++ Aereo.java
@@ -0,0 +1,129 @@
+import java.util.concurrent.Semaphore;
+
+public class Aereo {
+    int codiceVolo;
+    String impresaCostruttrice;
+    int maxPasseggeri;
+    int passeggieri;
+    int maxPesoBagagli;
+    int pesoBagagli;
+    boolean andataERitorno;
+    boolean serbatoioPieno;
+    Semaphore aereoSincro;
+
+    public Aereo(){
+        this.codiceVolo = 0;
+        this.impresaCostruttrice = "";
+        this.maxPasseggeri = 0;
+        this.passeggieri = 0;
+        this.maxPesoBagagli = 0;
+        this.pesoBagagli = 0;
+        this.serbatoioPieno = false;
+        this.andataERitorno = false;
+        this.aereoSincro = new Semaphore(0);
+    }
+
+    public Aereo(int codiceVolo, String impresaCostruttrice, int maxPasseggeri, int passeggieri, int maxPesoBagagli, boolean andataERitorno, boolean serbatoioPieno, int valoreSemaforo){
+        this.codiceVolo = codiceVolo;
+        this.impresaCostruttrice = impresaCostruttrice;
+        this.maxPasseggeri = maxPasseggeri;
+        this.passeggieri = passeggieri;
+        this.maxPesoBagagli = maxPesoBagagli;
+        this.pesoBagagli = 0;
+        this.andataERitorno = andataERitorno;
+        this.serbatoioPieno = serbatoioPieno;
+        this.aereoSincro = new Semaphore(valoreSemaforo);
+    }
+
+    public int getCodiceVolo() {
+        return codiceVolo;
+    }
+
+    public void setCodiceVolo(int codiceVolo) {
+        this.codiceVolo = codiceVolo;
+    }
+
+    public String getImpresaCostruttrice() {
+        return impresaCostruttrice;
+    }
+
+    public void setImpresaCostruttrice(String impresaCostruttrice) {
+        this.impresaCostruttrice = impresaCostruttrice;
+    }
+
+    public int getMaxPasseggeri() {
+        return maxPasseggeri;
+    }
+
+    public void setMaxPasseggeri(int maxPasseggeri) {
+        this.maxPasseggeri = maxPasseggeri;
+    }
+
+    public int getPasseggieri() {
+        return passeggieri;
+    }
+
+    public void setPasseggieri(int passeggieri) {
+        this.passeggieri = passeggieri;
+    }
+
+    public int getMaxPesoBagagli() {
+        return maxPesoBagagli;
+    }
+
+    public void setMaxPesoBagagli(int maxPesoBagagli) {
+        this.maxPesoBagagli = maxPesoBagagli;
+    }
+
+    public int getPesoBagagli() {
+        return pesoBagagli;
+    }
+
+    public void setPesoBagagli(int pesoBagagli) {
+        this.pesoBagagli = pesoBagagli;
+    }
+
+    public Semaphore getAereoSincro() {
+        return aereoSincro;
+    }
+
+    public void setAereoSincro(Semaphore aereoSincro) {
+        this.aereoSincro = aereoSincro;
+    }
+
+    public boolean getAndataERitorno() {
+        return andataERitorno;
+    }
+
+    public void setAndataERitorno(boolean andataERitorno) {
+        this.andataERitorno = andataERitorno;
+    }
+    
+    public boolean getSerbatoioPieno() {
+        return serbatoioPieno;
+    }
+
+    public void setSerbatoioPieno(boolean serbatoioPieno) {
+        this.serbatoioPieno = serbatoioPieno;
+    }
+
+    public void caricaPasseggieri(int passeggieriNuovi){
+        if(getPasseggieri() + passeggieriNuovi > getMaxPasseggeri()){
+            setPasseggieri(getMaxPasseggeri());
+        }else{
+            setPasseggieri(getPasseggieri() + passeggieriNuovi);
+        }
+    }
+
+    public void scaricaPasseggieri(){
+       setPasseggieri(0); 
+    }
+
+    public boolean prontoAlDecollo(){
+        return (getPasseggieri() == getMaxPasseggeri()) && (getPesoBagagli() == getMaxPesoBagagli()) && getSerbatoioPieno();
+    }
+
+
+
+    
+}
